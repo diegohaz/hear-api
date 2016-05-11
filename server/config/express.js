bb3b9de31702011bd2401f7b3794fb0e7e35bf67
Diego Haz <hazdiego@gmail.com>
2016-05-10 21:22:59 -0300

    Allow authorization header

--- server/config/express.js
+++ server/config/express.js
@@ -19,7 +19,7 @@ export default function(app) {
 
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
 
