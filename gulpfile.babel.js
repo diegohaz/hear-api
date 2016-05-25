d6e153691e83841642e374ed889098010d4450fa
Diego Haz <hazdiego@gmail.com>
2016-05-25 02:10:42 -0300

    Add build task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,24 @@
 import gulp from 'gulp'
+import del from 'del'
 import lazypipe from 'lazypipe'
 import loadPlugins from 'gulp-load-plugins'
 import runSequence from 'run-sequence'
 import {Instrumenter} from 'isparta'
 
 const paths = {}
-paths.src = 'src'
+paths.server = 'server'
 paths.dist = 'dist'
 paths.scripts = [
-  `${paths.src}/**/!(*.spec|*.integration).js`,
-  `!${paths.src}/config/local.env.sample.js`
+  `${paths.server}/**/!(*.spec|*.integration).js`,
+  `!${paths.server}/config/local.env.sample.js`
 ]
 paths.test = {
-  unit: [`${paths.src}/**/*.spec.js`, 'mocha.global.js'],
-  integration: [`${paths.src}/**/*.integration.js`, 'mocha.global.js']
+  unit: [`${paths.server}/**/*.spec.js`, 'mocha.global.js'],
+  integration: [`${paths.server}/**/*.integration.js`, 'mocha.global.js']
 }
 
 const plugins = loadPlugins()
+
 const mocha = lazypipe()
   .pipe(plugins.mocha, {
     reporter: 'spec',
@@ -38,6 +40,11 @@ const istanbul = lazypipe()
     coverageDirectory: './coverage',
     rootDirectory: ''
   })
+
+const babel = lazypipe()
+  .pipe(plugins.sourcemaps.init)
+  .pipe(plugins.babel)
+  .pipe(plugins.sourcemaps.write, '.')
 
 gulp.task('env:all', () => {
   const vars = require('./src/config/local.env')
@@ -84,6 +91,29 @@ gulp.task('test', (cb) => {
     'env:test',
     'test:unit',
     'test:integration',
+    cb
+  )
+})
+
+gulp.task('clean', () => {
+  return del([`${paths.dist}/!(.git*|Procfile)**`], {dot: true})
+})
+
+gulp.task('copy', () => {
+  return gulp.src('package.json')
+    .pipe(gulp.dest(paths.dist))
+})
+
+gulp.task('transpile', () => {
+  return gulp.src(paths.scripts)
+    .pipe(babel())
+    .pipe(gulp.dest(`${paths.dist}/${paths.server}`))
+})
+
+gulp.task('build', (cb) => {
+  runSequence(
+    'clean',
+    ['copy', 'transpile'],
     cb
   )
 })
