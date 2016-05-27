f0292ce4bf5a3e94b646ce52c56a6c6e00eea8cc
Diego Haz <hazdiego@gmail.com>
2016-05-26 22:35:14 -0300

    Update local.env.sample.js

--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -1,11 +1,6 @@
 'use strict'
 
-// Use local.env.js for environment variables that grunt will set when the server starts locally.
-// Use for your api keys, secrets, etc. This file should not be tracked by git.
-//
-// You will need to set these on the server you deploy to.
-
-module.exports = {
+export default {
   LASTFM_KEY: 'lastfmkey',
   GOOGLEMAPS_KEY: 'googlemapskey',
   FOURSQUARE_ID: 'foursquareid',
