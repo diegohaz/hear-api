6d08dc59307533565ec87ea44ad71d07f8af6a37
Haz <hazdiego@gmail.com>
2018-11-13 17:04:30 -0200

    Update server/index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,5 @@
 'use strict'
 
-const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
-if (env === 'development' || env === 'test') {
-  require('babel-core/register')
-}
+require('babel-core/register')
 
 exports = module.exports = require('./app')
